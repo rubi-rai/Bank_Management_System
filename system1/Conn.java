370501e05ba1e6903df2c9367bdebc9c9e369919
rubi-rai <57581516+rubi-rai@users.noreply.github.com>
2024-06-17 13:30:28 +0530

    Add files via upload

---
+++ system1/Conn.java
@@ -0,0 +1,23 @@
+
+package bank.management.system1;
+import java.sql.*;
+
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","root");
+            s=c.createStatement();
+            
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+}
